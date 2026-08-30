9f0ce0ca444becde61610414dc1a1cc6b976d165
jautry7 <27386573+jautry7@users.noreply.github.com>
2026-08-30 17:40:15 -0400

    chore: establish native app project structure

---
+++ Recompose/RecomposeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecomposeApp.swift
+//  Recompose
+//
+//  Created by John Autry on 8/30/26.
+//
+
+import SwiftUI
+
+@main
+struct RecomposeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
